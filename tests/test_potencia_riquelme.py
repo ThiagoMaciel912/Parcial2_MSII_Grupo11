6de12aea8f5c65c070c8e1d79d5a1838f5efb0d0
Thiago Maciel <thiagoagustinmaciel79@gmail.com>
2025-11-13 16:11:02 -0300

    Delete tests/test_potencia_riquelme.py

--- tests/test_potencia_riquelme.py
+++
@@ -1,7 +0,0 @@
-from funciones.potencia_riquelme import potencia
-
-def test_potencia():
-    assert potencia(2, 3) == 8
-    assert potencia(5, 0) == 1
-    assert potencia(-2, 3) == -8
-    assert potencia(0, 5) == 0
