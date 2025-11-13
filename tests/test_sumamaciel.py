40c2291593a40ad8648757ccf605a8ac8c3d28f3
Thiago Maciel <thiagoagustinmaciel79@gmail.com>
2025-11-13 15:37:13 -0300

    Update test_sumamaciel.py

--- tests/test_sumamaciel.py
+++ tests/test_sumamaciel.py
@@ -1,5 +1,5 @@
 from funciones.sumamaciel import sumamaciel
 
 def test_sumamaciel():
-    assert sumamaciel(3, 5) == 8
-    assert sumamaciel(-2, 2) == 0
+    assert sumamaciel(2, 3) == 5
+    assert sumamaciel(-1, 1) == 0
